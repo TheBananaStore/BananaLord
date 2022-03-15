07a6d6612aa412872d8f92e96a4a45605815c3e2
atwoyearold14 <simongileff@schoolathome.ca>
2022-03-14 19:09:01 -0700

    Added rich presence, meme command, random number gen, lock and unlock channels.

--- main.py
+++ main.py
@@ -15,10 +15,29 @@ slash = SlashCommand(client, sync_commands=True)
 
 @client.event
 async def on_ready():
-  print("BL logged on.")
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('under development'))
+    print("BL logged on.")
+
+#meme command, from r/memes subreddit.
+
+@slash.slash(
+    name="meme",
+    description="Displays a random meme from the r/Memes subreddit!",
+    guild_ids=[944368087526944778]
+)
+
+async def _meme(ctx:SlashContext):
+    embed = discord.Embed(title="", description="A random meme for your eyes.", color = (0xfff700))
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/Memes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
 
 
 #random number between 1 and 100
+
 @slash.slash(
     name="randomnum",
     description="Display a random number between 1 and 100!",
@@ -29,5 +48,36 @@ async def _randomnum(ctx:SlashContext):
     embed = discord.Embed(title = "Random Number :thumbsup:", description = (random.randint(1, 101)), color = (0xfff700))
     await ctx.send(embed = embed)
 
-client.run('OTUzMDkwMjE5MTgyMjYwMjg1.Yi_gbw.XldkfN5wKln9sOmpSqBd1kzEB5k')
+#lock a specified channel (the ability to manage channels is required.)
 
+@slash.slash(
+    name="lock",
+    description="Lock a specified channel, the ability to manage channels is required.",
+    guild_ids=[944368087526944778]
+)
+
+@commands.has_permissions(manage_channels=True)
+async def lock(ctx:SlashContext, channel : discord.TextChannel=None):
+    channel = channel or ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+    overwrite.send_messages = False
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send(':white_check_mark: Channel locked.')
+
+#unlock a specified channel (the ability to manage channels is required.)
+
+@slash.slash(
+    name="unlock",
+    description="Unlock a specified channel, the ability to manage channels is required.",
+    guild_ids=[944368087526944778]
+)
+
+@commands.has_permissions(manage_channels=True)
+async def lock(ctx:SlashContext, channel : discord.TextChannel=None):
+    channel = channel or ctx.channel
+    overwrite = channel.overwrites_for(ctx.guild.default_role)
+    overwrite.send_messages = True
+    await channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
+    await ctx.send(':white_check_mark: Channel unlocked.')
+
+client.run('OTUzMDkwMjE5MTgyMjYwMjg1.Yi_gbw.XldkfN5wKln9sOmpSqBd1kzEB5k')
