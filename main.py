4bccadb40d65f311afa4e3e3f0217d466aaf9cad
Grimet <97934952+atwoyearold14@users.noreply.github.com>
2022-03-18 10:55:35 -0700

    added serverinfo command

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from discord_slash.utils.manage_commands import create_choice, create_option
 
 client = commands.Bot(command_prefix="bl!")
 slash = SlashCommand(client, sync_commands=True)
+os.chdir(r'C:\Users\ITdep\Desktop\Coding Projects\Python\Banana Lord Bot')
 
 # Rich presence and status
 @client.event
@@ -319,6 +320,29 @@ async def ping(ctx: SlashContext):
         color=0xFFF700,
     )
     await ctx.send(embed=embed)
+
+#server info
+@slash.slash(
+    name="serverinfo",
+    description="Get info about this server!",
+    guild_ids=[944368087526944778]
+)
+
+async def serverinfo(ctx:SlashContext):
+    role_count = len(ctx.guild.roles)
+
+    serverinfoEmbed = discord.Embed(color=0xFFF700)
+    serverinfoEmbed.add_field(name='Name', value=f"{ctx.guild.name}", inline=False)
+    serverinfoEmbed.add_field(name='Member Count', value=ctx.guild.member_count, inline=False)
+    serverinfoEmbed.add_field(name='Verification Level', value=str(ctx.guild.verification_level), inline=False)
+    serverinfoEmbed.add_field(name='Highest Role', value=ctx.guild.roles[-2], inline=False)
+    serverinfoEmbed.add_field(name='Number of Roles', value=str(role_count), inline=False)
+
+    await ctx.send(embed = serverinfoEmbed)
+
+
+client.run(config.token)
+
 
 
 client.run(config.token)
